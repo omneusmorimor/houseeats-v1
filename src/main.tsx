e2e44856174102c3cf4e53556543d5df5b86874e
omneusmorimor <61885711+omneusmorimor@users.noreply.github.com>
2026-08-22 14:57:39 -0400

    Load responsive Chef layouts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./member-week.css";
 import "./houseeats-polish.css";
 import "./menu.css";
 import "./chef-polish.css";
+import "./chef-responsive.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
