74cb6bd097539ad5efc3cba52dd29dbdac3a723d
omneusmorimor <61885711+omneusmorimor@users.noreply.github.com>
2026-08-22 14:45:26 -0400

    Load HouseEats food-first visual polish

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import "./mobile-calendar.css";
 import "./member-week.css";
+import "./houseeats-polish.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
