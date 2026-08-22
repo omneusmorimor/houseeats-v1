250f3b1ed1246e0847cf1fcb1a88de36fe9ac26d
omneusmorimor <61885711+omneusmorimor@users.noreply.github.com>
2026-08-22 15:33:28 -0400

    Load final Chef launch polish after legacy styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./menu.css";
 import "./chef-polish.css";
 import "./chef-responsive.css";
 import "./chef-mobile-final.css";
+import "./chef-launch-polish.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
