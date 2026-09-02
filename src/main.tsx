b619b74be14f72495dc27c7baf2a28132b60f58b
omneusmorimor <61885711+omneusmorimor@users.noreply.github.com>
2026-09-01 22:09:17 -0400

    feat: load final V2 premium visual layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import "./v2.css";
 import "./v2-parity.css";
 import "./v2-overhaul.css";
+import "./v2-premium.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
