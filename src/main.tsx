d9fdf16e452408daef98e659e8754428b85764c2
omneusmorimor <61885711+omneusmorimor@users.noreply.github.com>
2026-09-05 08:03:13 -0400

    cleanup: load only active V2 global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import "./v2.css";
-import "./v2-parity.css";
-import "./v2-overhaul.css";
-import "./v2-premium.css";
 import "./v2-admin.css";
-import "./v2-parity-tools.css";
-import "./v2-rsvp-removal.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
